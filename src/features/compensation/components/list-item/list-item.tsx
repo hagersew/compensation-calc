4c9832ded50d8c70a44619a3764fa41e1df0e5a3
Abel Adane <contacttoabel@gmail.com>
2022-12-13 12:10:58 +0300

    refactored list item component

--- src/features/compensation/components/list-item/list-item.tsx
+++ src/features/compensation/components/list-item/list-item.tsx
@@ -3,17 +3,23 @@ export interface IListItemProps {
   children: React.ReactNode;
 }
 
+const BadgeColor = (color: string) => {
+  return (
+    <div
+      className="my-2 mx-2"
+      style={{
+        width: '16px',
+        height: '4px',
+        backgroundImage: color,
+      }}
+    />
+  );
+};
+
 const ListItem = ({ color, children }: IListItemProps) => {
   return (
     <div className="flex">
-      <div
-        className="my-2 mx-2"
-        style={{
-          width: '16px',
-          height: '4px',
-          backgroundImage: color,
-        }}
-      ></div>
+      {BadgeColor(color!)}
       {children}
     </div>
   );
